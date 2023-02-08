eff71c472064baeb8f1cb7577bde2dc586299295
mamonde456 <mamonde456@gmail.com>
2023-02-08 16:21:18 +0900

    update: typescript로 변환 > interface 사용하여 설계

---
+++ src/index.ts
@@ -0,0 +1,41 @@
+interface ITags {
+  id: string;
+  tag: string;
+}
+interface IToDo {
+  id: string;
+  content: string;
+  category: "DOING" | "DONE" | "TODO";
+  isFinish: boolean;
+  tags?: ITags[];
+}
+
+function createToDo(todo: IToDo) {
+  return;
+}
+
+interface IReadToDo {
+  id?: string;
+}
+
+function readToDo({ id }: IReadToDo) {
+  return;
+}
+
+interface IUpdateToDo {
+  id: string;
+  tag: ITags;
+}
+
+function updateToDo(id: IUpdateToDo, tag: IUpdateToDo) {
+  return;
+}
+
+interface IDeleteToDo {
+  id: string;
+  tags: ITags[];
+}
+
+function deleteToDo(id: IDeleteToDo, tags: IDeleteToDo) {
+  return;
+}
